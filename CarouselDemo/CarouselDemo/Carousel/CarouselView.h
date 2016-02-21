9885fe6be9921e4f05b76c246cfba18f11cc34ac
JaySonGD <czljcb@163.com>
2016-02-21 20:21:47 +0800

    添加轮播View

---
+++ CarouselDemo/CarouselDemo/Carousel/CarouselView.h
@@ -0,0 +1,13 @@
+//
+//  CarouselView.h
+//  CarouselDemo
+//
+//  Created by czljcb on 16/2/21.
+//  Copyright © 2016年 czljcb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CarouselView : UIView
+
+@end
---
+++ CarouselDemo/CarouselDemo/Carousel/CarouselView.m
@@ -0,0 +1,21 @@
+//
+//  CarouselView.m
+//  CarouselDemo
+//
+//  Created by czljcb on 16/2/21.
+//  Copyright © 2016年 czljcb. All rights reserved.
+//
+
+#import "CarouselView.h"
+
+@implementation CarouselView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
